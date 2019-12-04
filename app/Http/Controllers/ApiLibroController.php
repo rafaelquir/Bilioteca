e09756284dcb3177aa69aded445dd3e8fae414bd
Iván Obejo <ivanobejo@hotmail.es>
2019-12-04 20:34:40 +0100

    completando el código

--- app/Http/Controllers/ApiLibroController.php
+++ app/Http/Controllers/ApiLibroController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Libro;
 class ApiLibroController extends Controller
 {
-    //
+    public function index()
+    {
+         $libros = new libro();
+    }
 }
